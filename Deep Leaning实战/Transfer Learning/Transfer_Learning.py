46f559692aa5776b33dac0322e3a957796e8a931
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2022-02-26 16:10:24 +0800

    Update Transfer_Learning.py

--- Deep Leaning实战/Transfer Learning/Transfer_Learning.py
+++ Deep Leaning实战/Transfer Learning/Transfer_Learning.py
@@ -1,8 +1,10 @@
 import os
 import torch
 import torchvision
+import torch.nn as nn
 import matplotlib.pyplot as plt
-from torchvision import datasets, transforms
+from torch import optim
+from torchvision import datasets, transforms, models
 
 
 """
@@ -19,7 +21,44 @@ def imshow(inputs):
     plt.imshow(inputs)
     plt.show()
 
+"""
+定义迁移学习的训练函数
+"""
+def train(model, criterion, optimizer, epochs=1):
+    for epoch in range(epochs):
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
+            running_loss += loss.item()
+            if i % 10 == 9:
+                print('[Epoch: %d, Batch: %5d] Loss: %.3f' % (epoch+1, i+1, running_loss/100))
+                running_loss = 0.0
+    print('Finished Training')
+
+
+def test(testloader, model):
+    correct = 0
+    total = 0
+    for data in testloader:
+        images, labels = data
+        outputs = model.forward(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum()
+    print('Accuracy on the test set: %d %%' %(100*correct/total))
+
+def save_param(model, path):
+    torch.save(model.state_dict(), path)
+
+def load_param(model, path):
+    if os.path.exists(path):
+        model.load_state_dict(torch.load(path))
 
 """
 数据预处理部分:
@@ -49,9 +88,14 @@ data_transforms = {
     ])
 }
 
+"""
+数据加载部分
+
+训练与测试数据放置于`data_set`文件夹内
+    注意: `data_set`文件夹需要放置在路径os.getcwd()下
+"""
 # 数据集文件夹路径
 data_directory = 'data_set'
-
 trainset = datasets.ImageFolder(os.path.join(data_directory, 'train'), data_transforms['train'])
 testset = datasets.ImageFolder(os.path.join(data_directory, 'test'), data_transforms['test'])
 
@@ -60,3 +104,44 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=5, shuffle=True, nu
 
 inputs, classes = next(iter(trainloader))
 imshow(torchvision.utils.make_grid(inputs))
+
+"""
+使用经典的AlexNet模型
+pretrained=True代表加载经过了ImageNet数据集训练后的模型参数
+print(alexnet)可以直接打印模型的结构
+
+AlexNet模型结构:
+    1. features模块: 负责提取特征
+    2. classifier模块: 负责实现分类, 以全连接层为主
+
+本实验为二分问题, 因此需要重新定义AlexNet的classifier模块,
+将最后一层的输出改为2
+"""
+alexnet = models.alexnet(pretrained=True)
+
+for param in alexnet.parameters():
+    # 限制参数的更新, 保证只更新classifier的参数
+    param.requires_grad = False
+
+alexnet.classifier=nn.Sequential(
+    nn.Dropout(),
+    nn.Linear(256*6*6, 4096),
+    nn.ReLU(inplace=True),
+    nn.Dropout(),
+    nn.Linear(4096, 4096),
+    nn.ReLU(inplace=True),
+    nn.Linear(4096, 2),
+)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(alexnet.classifier.parameters(), lr=0.001, momentum=0.9)
+
+"""
+ -----------------------------------------------
+|             AlexNet训练以及测试部分              |
+ -----------------------------------------------
+"""
+load_param(alexnet, 'tl_model.pkl')
+train(alexnet, criterion, optimizer, epochs=2)
+save_param(alexnet, 'tl_model.pkl')
+test(testloader, alexnet)
