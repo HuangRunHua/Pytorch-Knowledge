93f3233fab88a33b76b75ca597ee71194ee978a7
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2022-03-01 19:42:57 +0800

    Update GAN.py

--- Deep Leaning实战/GAN/GAN.py
+++ Deep Leaning实战/GAN/GAN.py
@@ -205,6 +205,48 @@ class G(nn.Module):
         out = self.layer4(out)
         return out
 
+def train(d, g, criterion, d_optimizer, g_optimizer, epochs=1, show_every=1000, print_every=10):
+    iter_count = 0
+    for epoch in range(epochs):
+        for inputs, _ in trainloader:
+            real_inputs = inputs
+            fake_inputs = g(torch.rand(5, 100))
+
+            real_labels = torch.ones(real_inputs.size(0))
+            fake_labels = torch.zeros(5)
+
+            real_outputs = d(real_inputs)
+            d_loss_real = criterion(real_outputs, real_labels)
+            real_scores = real_outputs
+
+            fake_outputs = d(fake_inputs)
+            d_loss_fake = criterion(fake_outputs, fake_labels)
+            fake_scores = fake_outputs
+
+            d_loss = d_loss_real + d_loss_fake
+            d_optimizer.zero_grad()
+            d_loss.backward()
+            d_optimizer.step()
+
+            fake_inputs = g(torch.randn(5, 100))
+            outputs = d(fake_inputs)
+            real_labels = torch.ones(outputs.size(0))
+            g_loss = criterion(outputs, real_labels)
+
+            g_optimizer.zero_grad()
+            g_loss.backward()
+            g_optimizer.step()
+
+            if (iter_count % show_every == 0):
+                print('Epoch: {}, Iter: {}, D: {:.4}, G: {:.4}'.format(epoch, iter_count, d_loss.item(), g_loss.item()))
+                pic_name = "Epoch_" + str(epoch) + "Iter_" + str(iter_count)
+                imshow(torchvision.utils.make_grid(fake_inputs.date), pic_name)
+
+            if (iter_count % print_every == 0):
+                print('Epoch: {}, Iter: {}, D: {:.4}, G: {:.4}'.format(epoch, iter_count, d_loss.item(), g_loss.item()))
+                pic_name = "Epoch_" + str(epoch) + "Iter_" + str(iter_count)
+            iter_count += 1
+
 
 data_transform = transforms.Compose([
     transforms.Resize((96, 96)),
@@ -218,3 +260,12 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=5,shuffle=True, n
 
 inputs, _ = next(iter(trainloader))
 imshow(torchvision.utils.make_grid(inputs), "RealDataSample")
+
+d = D(3, 32)
+g = G(3, 128, 1024, 100)
+
+criterion = nn.BCELoss()
+d_optimizer = torch.optim.Adam(d.parameters(), lr=0.0003)
+g_optimizer = torch.optim.Adam(g.parameters(), lr=0.0003)
+
+train(d, g, criterion, d_optimizer, g_optimizer, epochs=300)
